5e7bb63e5c85d029b069d2259b3ff6ace8ce77ca
Anurag Raut <rautab0@gmail.com>
2024-04-30 19:02:26 +0530

    uploading file to bucket

--- packages/backend/main.go
+++ packages/backend/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
+
 	// "fmt"
 	"io"
-	"log"
+
 	"net/http"
+
 	// "os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gin-gonic/gin"
+	"github.com/gofor-little/env"
 )
 
 type BucketBasics struct {
@@ -24,14 +29,25 @@ func (basics *BucketBasics) UploadFile(bucketName string, objectKey string, file
 		Key:    aws.String(objectKey),
 		Body:   file,
 	})
+
 	return err
 }
 
 func main() {
+	env.Load(".env")
 	// Initialize AWS session
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	var AWS_ACCESS_KEY = env.Get("AWS_ACCESS_KEY", "")
+	var AWS_SECRET_KEY = env.Get("AWS_SECRET_KEY", "")
+	fmt.Println(AWS_ACCESS_KEY, AWS_SECRET_KEY, "printt")
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			AWS_ACCESS_KEY,
+			AWS_SECRET_KEY,
+			"",
+		)),
+		config.WithRegion("ap-south-1"))
 	if err != nil {
-		log.Fatalf("unable to load AWS config, %v", err)
+		panic("configuration error, " + err.Error())
 	}
 
 	// Create S3 client
@@ -43,19 +59,19 @@ func main() {
 	// Handle file upload
 	r.POST("/upload", func(c *gin.Context) {
 		file, header, err := c.Request.FormFile("file")
-		
+
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "No file uploaded"})
 			return
 		}
 		defer file.Close()
 
-		bucketName := "your-bucket-name"
+		bucketName := "coursecrafter"
 		objectKey := header.Filename
 
 		err = basics.UploadFile(bucketName, objectKey, file)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
