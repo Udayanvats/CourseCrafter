47ed6b2ae330a47a587a8f59a0c6e3d7c8e3b1a6
Anurag Raut <rautab0@gmail.com>
2024-05-18 12:30:47 +0530

    buildup complete

--- packages/frontend/app/_components/buildupContent/study.tsx
+++ packages/frontend/app/_components/buildupContent/study.tsx
@@ -1,21 +1,20 @@
 "use client"
-
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { AiOutlineFilePpt } from "react-icons/ai";
 import { FaRegFilePdf } from "react-icons/fa";
-import { RiBardLine } from "react-icons/ri";
-import {motion} from "framer-motion"
+import { FaUpload } from "react-icons/fa6";
 
-export default function ConvertContent() {
+
+export default function StudyContent() {
     return (
-        <motion.div 
+        <motion.div
         exit={{
             x: -1000,
             // scale:0.7,
             opacity: 0,
           }}
-          key="purple"
-
+         
           initial={{
             x: 1000,
             // scale:0.7,
@@ -30,11 +29,10 @@ export default function ConvertContent() {
           transition={{
             duration: 0.3,
           }}
-
-        className="px-20 pb-6 pt-6  rounded-lg mx-12  mt-16 ">
+        className="px-20 pb-6 pt-6 shadow-lg    rounded-lg mx-12  mt-16 ">
             <div className="h-[500px] ">
                 <div className="text-2xl  font-bold tracking-wide text-gray-500">
-                    <span className="text-white">CONVERT. </span>
+                    <span className="text-white">BUILD.</span>
                     STREAMLINE YOUR DEVELOPMENT
 
                 </div>
@@ -47,21 +45,24 @@ export default function ConvertContent() {
 
                             <div className=" bg-blue-950 p-4 w-fit rounded-lg">
 
-                                <RiBardLine size={55} className="fill-blue-500" />
+                                <AiOutlineFilePpt size={55} className="fill-blue-500" />
                             </div>
-                          
+                            <div className=" bg-orange-950 p-4 w-fit rounded-lg ml-3">
+
+                                <FaRegFilePdf size={55} className="fill-orange-500" />
+                            </div>
 
                             <div className="text-white text-2xl font-bold ml-3">
-                                Convert your Documents
+                                Upload your Documents
                             </div>
                         </div>
                         <div className="mt-6 text-[18px]">
-                            We Use the latest AI technology to convert your ppts into easy to understand notes. This will help you to understand the content better and also help you to revise the content easily.
+                            We Accept both ppts and pdfs. Upload your documents and we will extract the text from them to generate best course for you.
                         </div>
 
                     </div>
                     <div className="  w-fit h-fit m-3 rounded-xl overflow-hidden">
-                        <Image src={"/convertPpt.png"} alt={"upload"} width={720} height={700} />
+                        <Image src={"/uploadPpts.png"} alt={"upload"} width={820} height={700} />
 
                     </div>
 
--- packages/frontend/app/_components/landingpage/buildup.tsx
+++ packages/frontend/app/_components/landingpage/buildup.tsx
@@ -4,6 +4,11 @@ import React, { useEffect, useRef, useState } from "react";
 import StickyBox from "react-sticky-box";
 import UploadContent from "../buildupContent/upload";
 import ConvertContent from "../buildupContent/convert";
+import StudyContent from "../buildupContent/study";
+
+function scaleValue(value: number, oldMin: number, oldMax: number, newMin: number, newMax: number) {
+  return ((value - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
+}
 
 const Buildup = () => {
   const [scrollY, setScrollY] = useState(0);
@@ -17,24 +22,25 @@ const Buildup = () => {
       setScrollY(window.scrollY);
       const winHeight = 2000;
       const docHeight = document.documentElement.scrollHeight;
-      const scrollPercent = ((window.scrollY-1000)/2000)*100;
-      console.log(((window.scrollY-1000)/2000)*100)
-      const containerWidth = containerRef.current ? containerRef?.current?.clientWidth+1300 : 0;
-      console.log(containerWidth,"container")
-      if(scrollPercent<0){
+      const scrollPercent = ((window.scrollY - 1000) / 2000) * 100;
+      console.log(((window.scrollY - 1000) / 2000) * 100)
+      const containerWidth = containerRef.current ? containerRef?.current?.clientWidth : 0;
+      console.log(containerWidth, "container")
+      if (scrollPercent < 0) {
       }
       if (scrollPercent < 33.333333333333) {
-        console.log((scrollPercent/100)*containerWidth)
-        setProgressWidth1((scrollPercent / 100) * containerWidth);
+        console.log((scrollPercent / 100) * containerWidth)
+
+        setProgressWidth1((scaleValue(scrollPercent, 0, 33.333333333333, 0, 100) / 100) * containerWidth);
         setProgressWidth2(0);
         setBreakpoint(0);
-      } else if (scrollPercent >= 33.33 && scrollPercent < 66.66) {
-        setProgressWidth1((33.33 / 100) * containerWidth);
-        setProgressWidth2(((scrollPercent - 33.33) / 100) * containerWidth);
+      } else if (scrollPercent >= 33.33 && scrollPercent < 66.66666666) {
+        setProgressWidth1((scaleValue(33.333333333333, 0, 33.333333333333, 0, 100) / 100) * containerWidth);
+        setProgressWidth2(((scaleValue(scrollPercent - 33.3333333, 0, 33.333333333333, 0, 100)) / 100) * containerWidth + 5);
         setBreakpoint(1);
       } else {
-        setProgressWidth1((33.33 / 100) * containerWidth);
-        setProgressWidth2((33.33 / 100) * containerWidth);
+        setProgressWidth1((scaleValue(33.333333333333, 0, 33.333333333333, 0, 100) / 100) * containerWidth);
+        setProgressWidth2(((scaleValue(66.66666666 - 33.3333333, 0, 33.333333333333, 0, 100)) / 100) * containerWidth + 5);
         setBreakpoint(2);
       }
     };
@@ -47,60 +53,70 @@ const Buildup = () => {
   }, []);
 
   return (
-      <div className="row h-[2500px]">
-        <StickyBox offsetTop={20} offsetBottom={20}>
-          <div>
-            <h1 className="font-bold text-white px-10 text-3xl text-center mb-10">
-              Empower Students with Dynamic Learning Experiences
-            </h1>
-            <h3 className="text-center px-80 text-md text-gray font-semibold">
-              Revolutionize your study experience by effortlessly converting PPTs
-              into interactive courses. Our intuitive platform streamlines the
-              process, empowering active learning and unlocking academic potential.
-            </h3>
-          </div>
-          <div className="border border-white b-2">
-            <div className="px-16 mt-6" >
-              <div className="flex justify-between items-center text-white text-2xl font-bold">
-                <span>Build</span>
-                <div id="build" ref={containerRef} className="relative h-2 w-full bg-red-500 rounded-full mx-4">
-                  <div
-                    className="absolute h-2 bg-white rounded-full progress-bar"
-                    style={{ width: `${progressWidth1}px` }}
-                  ></div>
+    <div className="row ">
+      <div className="mb-[100px]">
+        <h1 className="font-bold text-white px-10 text-3xl text-center mb-10">
+          Empower Students with Dynamic Learning Experiences
+        </h1>
+        <h3 className="text-center px-80 text-md text-gray font-semibold">
+          Revolutionize your study experience by effortlessly converting PPTs
+          into interactive courses. Our intuitive platform streamlines the
+          process, empowering active learning and unlocking academic potential.
+        </h3>
+      </div>
+      <div className="row h-[2350px]">
+
+        <StickyBox offsetTop={0} offsetBottom={0} className="flex justify-center w-full bg-secondaryBackground h-screen justify-center flex items-center">
+          <div className="overflow-hidden flex justify-center  w-[89%]  bg-[linear-gradient(to_right,rgba(25,25,25,0)_0%,rgba(25,25,25,1)_20%,rgba(25,25,25,1)_25%,rgba(25,25,25,1)_25%,rgba(25,25,25,1)_35%,rgba(25,25,25,1)_35%,rgba(25,25,25,1)_50%,rgba(25,25,25,1)_65%,rgba(25,25,25,1)_65%,rgba(25,25,25,1)_75%,rgba(25,25,25,1)_75%,rgba(25,25,25,1)_80%,rgba(25,25,25,0)_100%)] ">
+            {/* <div className="w-[155px] h-5 bg-white">
+
+</div> */}
+            <div className=" w-[90%]  ">
+
+              <div className="px-16 mt-6" >
+                <div className="flex justify-between items-center  text-2xl font-bold">
+                  <span className={`text-5xl tracking-tighter ${breakpoint>=0&& "text-white"}`}>UPLOAD</span>
+                  <div id="build" ref={containerRef} className="relative h-1 w-full bg-gray-500 rounded-full mx-4">
+                    <div
+                      className="absolute h-1 bg-white rounded-full progress-bar"
+                      style={{ width: `${progressWidth1}px` }}
+                    ></div>
+                  </div>
+                  <span className={`text-5xl tracking-tighter ${breakpoint>=1&& "text-white"}`}>CONVERT</span>
+                  <div className="relative h-1 w-full bg-gray-500 rounded-full mx-4">
+                    <div
+                      className="absolute h-1 bg-white rounded-full progress-bar"
+                      style={{ width: `${progressWidth2}px` }}
+                    ></div>
+                  </div>
+                  <span className={`text-5xl tracking-tighter ${breakpoint>=2&& "text-white"}`}>STUDY</span>
                 </div>
-                <span>Fortify</span>
-                <div className="relative h-2 w-full bg-gray-500 rounded-full mx-4">
-                  <div
-                    className="absolute h-2 bg-white rounded-full progress-bar"
-                    style={{ width: `${progressWidth2}px` }}
-                  ></div>
-                </div>
-                <span>Grow</span>
               </div>
-            </div>
-          </div>
-          <div className="w-full">
-            <AnimatePresence mode="wait">
-              {
-                breakpoint === 0
-                  ?
-                  <UploadContent />
-                  :
-                  breakpoint === 1
+              {/* <div className="w-full"> */}
+              <AnimatePresence mode="wait" >
+                {
+                  breakpoint === 0
                     ?
-                    <ConvertContent />
+                    <UploadContent key={breakpoint} />
                     :
-                    <UploadContent />
+                    breakpoint === 1
+                      ?
+                      <ConvertContent key={breakpoint} />
+                      :
+                      <StudyContent key={breakpoint} />
 
 
-              }
+                }
 
-            </AnimatePresence>
 
+              </AnimatePresence>
+
+              {/* </div> */}
+            </div>
           </div>
         </StickyBox>
       </div>
+    </div>
   );
 };
 
