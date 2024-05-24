3d6dfaf13cbdd4eb84acfb944b5c493b41d41258
udayanvats <udayan.asus06@gmail.com>
2024-05-24 19:16:39 +0530

    Did some changes

--- packages/frontend/app/_components/landingpage/accordion.tsx
+++ packages/frontend/app/_components/landingpage/accordion.tsx
@@ -12,7 +12,7 @@ const Accordion = ({ title, content }: AccordionProps) => {
   };
 
   return (
-    <div className=" rounded-md overflow-hidden">
+    <div className=" rounded-lg overflow-hidden shadow-md border-gray-50 ">
       <div
         className={`flex justify-between items-center  py-3 cursor-pointer ${
           isOpen ? "bg-transparent" : "bg-transparent"
