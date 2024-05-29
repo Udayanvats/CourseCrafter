ef3c48498e03297d2cd72f4c896cf880cb7a07d4
udayanvats <udayan.asus06@gmail.com>
2024-05-29 15:19:02 +0530

    TESTING RUNNER

--- packages/frontend/app/_components/landingpage/accordion.tsx
+++ packages/frontend/app/_components/landingpage/accordion.tsx
@@ -14,7 +14,7 @@ const Accordion = ({ title, content }: AccordionProps) => {
   return (
     <div className=" rounded-lg overflow-hidden ">
       <div
-        className={`flex justify-between items-center  py-4 cursor-pointer ${
+        className={`flex justify-between items-center  py-1 cursor-pointer ${
           isOpen ? "bg-transparent" : "bg-transparent"
         }`}
         onClick={toggleAccordion}
